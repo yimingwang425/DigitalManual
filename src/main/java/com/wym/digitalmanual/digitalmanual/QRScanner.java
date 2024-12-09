2e5170a6badaee6d428dc83e40bea45610dff67e
YIMING WANG <yxw1268@student.bham.ac.uk>
2024-12-09 22:35:33 +0000

    Add QRScanner implementation

--- src/main/java/com/wym/digitalmanual/digitalmanual/QRScanner.java
+++ src/main/java/com/wym/digitalmanual/digitalmanual/QRScanner.java
@@ -1,4 +1,14 @@
 package com.wym.digitalmanual.digitalmanual;
 
 public class QRScanner {
+
+    public static String scanQRCode() {
+        // Simulate scanning of QR code, to be implemented
+        return "SSH001";
+    }
+
+    public static String inputDeviceId(String input) {
+        return input;
+    }
+
 }
