73538dee31b4d657f1d9d90aac3852d59ad564ce
YIMING WANG <yxw1268@student.bham.ac.uk>
2024-12-09 22:52:14 +0000

    Add DeviceRecognitionTest.java implementation

--- src/main/java/com/wym/digitalmanual/digitalmanual/DeviceRecognitionTest.java
+++ src/main/java/com/wym/digitalmanual/digitalmanual/DeviceRecognitionTest.java
@@ -1,4 +1,9 @@
 package com.wym.digitalmanual.digitalmanual;
 
 public class DeviceRecognitionTest {
+    public static void main(String[] args) {
+        String deviceId = QRScanner.scanQRCode();
+        boolean isValid = DeviceRecognition.validateDevice(deviceId);
+        System.out.println("Device valid: " + isValid);
+    }
 }
